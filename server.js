1b790bdb1f1cddf32d0088375cf617a1e0343ddb
jdstherapy <jdstherapy@outlook.com>
2026-08-18 15:31:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 const allowedOrigins = [
   'http://127.0.0.1:5000',
   'https://boisterous-queijadas-409afa.netlify.app',
-  'https://rexxiez.netlify.app',
+  'https://millt.netlify.app',
   'https://billbelll.netlify.app',
    // add all your trusted frontends here
 ];

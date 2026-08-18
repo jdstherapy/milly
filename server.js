5281fe69af6fbadc59464feb244c901363a4d58a
jdstherapy <jdstherapy@outlook.com>
2026-08-18 11:56:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5000',
   'https://boisterous-queijadas-409afa.netlify.app',
   'https://rexxiez.netlify.app',
-  'https://bellbusinesscanada.netlify.app',
-  'https://sunny-pastelito-8843b7.netlify.app',
-  'https://viralbell.netlify.app',
-  'https://jhvnb.vercel.app',
+  'https://billbelll.netlify.app',
    // add all your trusted frontends here
 ];
 

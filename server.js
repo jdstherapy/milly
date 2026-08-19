77bceceed283c0581a7c61ad82d4b93989e192c9
jdstherapy <jdstherapy@outlook.com>
2026-08-19 19:28:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 const allowedOrigins = [
   'http://127.0.0.1:5000',
   'https://belbil.netlify.app',
-  'https://millt.netlify.app',
+  'https://belbil.netlify.app',
   'https://billbelll.netlify.app',
    // add all your trusted frontends here
 ];
